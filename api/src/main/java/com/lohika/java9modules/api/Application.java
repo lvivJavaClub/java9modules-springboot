1d9084585642b81be27f368f47c6802fd52d28dc
Roman Tsypuk <rtsypuk@lohika.com>
2017-04-27 00:09:58 +0300

    added test deps

--- api/src/main/java/com/lohika/java9modules/api/Application.java
+++ api/src/main/java/com/lohika/java9modules/api/Application.java
@@ -1,14 +1,18 @@
 package com.lohika.java9modules.api;
 
+import com.lohika.java9modules.user.UserServiceConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan(value = {
     "com.lohika.java9modules.api",
-    "com.lohika.java9modules.user.service"
+    "com.lohika.java9modules.user"
 })
+@Import(UserServiceConfiguration.class)
 public class Application {
 
   public static void main(String[] args) {
--- user-service/src/main/java/com/lohika/java9modules/user/UserServiceConfiguration.java
+++ user-service/src/main/java/com/lohika/java9modules/user/UserServiceConfiguration.java
@@ -1,13 +1,11 @@
 package com.lohika.java9modules.user;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@PropertySource("service.properties")
+@PropertySource(value = "service.properties")
 @Configuration
-@ComponentScan("com.lohika.java9modules.user.service")
 @EnableMongoRepositories("com.lohika.java9modules.user.service")
 public class UserServiceConfiguration {
 }
